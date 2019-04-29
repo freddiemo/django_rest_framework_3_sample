971638720cbf3cb5ba25a623dc293824939634ea
Freddie <freddiejmo@gmail.com>
2019-04-28 21:36:07 -0400

    changed login import to the browsable API

--- django_rest_framework_3_sample/urls.py
+++ django_rest_framework_3_sample/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from quickstart import views
 from rest_framework import routers
+from django.conf.urls import include
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -26,7 +27,6 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include(
-        'rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls')),
     path('', include('snippets.urls'))
 ]
